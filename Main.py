e3c712f635e5c8a2b5dd40378c3d27d44d3ba8ab
Viann Cheng <46937676+vivchc@users.noreply.github.com>
2023-07-12 00:02:56 -0400

    feat: Edit tasks, help menu

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import TaskAppDriver
 """Runs the main application."""
 
 driver = TaskAppDriver.TaskAppDriver()  # calls driver class
+print("Type 'help' for to see cmds.")
 
 
 def ask_user():
@@ -15,8 +16,28 @@ def ask_user():
             exit()
         elif user_input.lower() == "show":
             driver.show_all_tasks()
+        elif user_input.lower() == "help":
+            help_key()
+        elif user_input.lower() == "clear":
+            driver.clear_file()
+            print("Cleared all tasks!")
         elif user_input.lower().startswith("add"):
-            driver.add_task(user_input)
+            driver.add_task(user_input.split()[1])  # Leave "add" cmd out
+            print("Added")
+        elif user_input.lower().startswith("edit"):
+            driver.edit_task(user_input.split()[1])  # Leave "edit" cmd out
+
+
+def help_key():
+    key = """
+    Add tasks: add <task>
+    Edit tasks: edit <task #>
+    Clear all tasks: clear
+    Show tasks: show
+    Show help key: help
+    Exit app: quit or q
+    """
+    print(key)
 
 
 ask_user()
--- TaskAppDriver.py
+++ TaskAppDriver.py
@@ -1,17 +1,57 @@
+from queue import Queue
+
+
 class TaskAppDriver:
     """Actions in task app."""
 
     def add_task(self, task):
-        """Numbers new task and appends it to the end of file."""
+        """Numbers new task and appends to file."""
         task_num = self.total_num_of_tasks()  # 0-based numbering
         file = open("tasklist.txt", "a")
-        file.write(f"{task_num} {task[4:]}\n")  # Leave "add" cmd out
+        file.write(f"{task_num} {task}\n")
+
+    def edit_task(self, n):
+        """Edits the nth task."""
+        file = open("tasklist.txt", "r")
+        q = Queue(maxsize=0)  # infinite size
+        not_found = 0  # Tracks if nth task exists
+
+        # If task exists, let user edit
+        for task in file.readlines():
+            task_num = task.split()[0]
+
+            # Puts tasks into queue. Only inserts task, numbering added later.
+            if task_num == n:
+                # Put new task into queue
+                task = input(f"New task #{n}: ")
+            else:
+                task = task.split()[1]
+
+            q.put(task)
+
+        if not_found == 1:
+            print("Task not found.")
+        else:
+            self.rewrite_file(q)
+
+    def rewrite_file(self, q):
+        """Rewrites file given a queue of tasks (FIFO)."""
+        # Clears file
+        self.clear_file()
+        # Add tasks from queue into file
+        while not q.empty():
+            self.add_task(q.get())
+
+    def clear_file(self):
+        file = open("tasklist.txt", "r+")
+        file.seek(0)
+        file.truncate()  # truncates file to 0th byte
 
     def show_all_tasks(self):
         """Prints out all tasks on file."""
         file = open("tasklist.txt", "r")
-        for line in file.readlines():
-            print(f"{line[:-1]}")  # Leaves out newline char
+        for task in file.readlines():
+            print(f"{task[:-1]}")  # Leaves out newline char
 
     def total_num_of_tasks(self):
         """Returns the total number of tasks currently on file."""
@@ -20,4 +60,7 @@ class TaskAppDriver:
 
     def close_file(self):
         """Closes file."""
-        open("tasklist.txt", "r").close()
+        try:
+            open("tasklist.txt", "r").close()  # Read mode so file not overwritten
+        except FileNotFoundError:
+            None  # File doesn't exist
